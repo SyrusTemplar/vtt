a42a372c794e1d4c3a9528030352e97393c8b4d1
SyrusTemplar <79186347+SyrusTemplar@users.noreply.github.com>
2022-02-14 10:32:20 +0200

    vtt

---
+++ docs/MyVTT/Client/app/routes.py
@@ -0,0 +1,14 @@
+from app import app
+from flask import render_template
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/login')
+def login():
+    return "TODO"
+
---
+++ docs/MyVTT/README.md
@@ -0,0 +1,8 @@
+# MyVTT
+
+### I suggest using PyCharm for work on the client.
+
+### Flask + Javascript
+
+
+The webpage will likely be mainly used as a gui and will probably be completly integrated in the flask application.
