02ecee961339097b311b57fd3a95f7ccf35995fb
SyrusTemplar <79186347+SyrusTemplar@users.noreply.github.com>
2022-03-05 09:16:03 +0200

    Update jscripts.js

--- docs/js/jscripts.js
+++ docs/js/jscripts.js
@@ -28,5 +28,30 @@ function includeHTML() {
   }
 };
 
+// function to set a given theme/color-scheme
+function setTheme(themeName) {
+    localStorage.setItem('theme', themeName);
+    document.documentElement.className = themeName;
+}
+
+// function to toggle between light and dark theme
+function toggleTheme() {
+    if (localStorage.getItem('theme') === 'theme-light') {
+        setTheme('theme-dark');
+    } else {
+        setTheme('theme-light');
+    }
+}
+
+// Immediately invoked function to set the theme on initial load
+(function () {
+    if (localStorage.getItem('theme') === 'theme-light') {
+        setTheme('theme-light');
+    } else {
+        setTheme('theme-dark');
+    }
+})();
+
+document.getElementById("year").innerHTML = new Date().getFullYear();
 
 
