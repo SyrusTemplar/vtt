5cc224ecd524c068d6f7016cbeb281770d031c1a
SyrusTemplar <79186347+SyrusTemplar@users.noreply.github.com>
2022-06-17 15:50:21 +0300

    Opened AD&D complete monster's manual

---
+++ docs/2e/mm/jscripts.js
@@ -0,0 +1,25 @@
+
+/* FUNCTIONS FOR COLOR SWITCHING */
+
+// function to set a given theme/color-scheme
+function setTheme(themeName) {
+    localStorage.setItem('theme', themeName);
+    document.documentElement.className = themeName;
+}
+// function to toggle between light and dark theme
+function toggleTheme() {
+    if (localStorage.getItem('theme') === 'theme-light') {
+        setTheme('theme-dark');
+    } else {
+        setTheme('theme-light');
+    }
+}
+// Immediately invoked function to set the theme on initial load
+(function () {
+    if (localStorage.getItem('theme') === 'theme-light') {
+        setTheme('theme-light');
+    } else {
+        setTheme('theme-dark');
+    }
+})();
+
