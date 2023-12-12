fb6da801134d68c2591ac3456b322dc9917726b4
SyrusTemplar <79186347+SyrusTemplar@users.noreply.github.com>
2023-12-12 15:51:28 +0200

    Update jscripts.js

--- docs/js/jscripts.js
+++ docs/js/jscripts.js
@@ -52,3 +52,11 @@ function toggleTheme() {
         setTheme('theme-dark');
     }
 })();
+
+
+/* VARIOUS FUNCTIONS */
+
+var year = new Date().getFullYear();
+document.getElementById('copyright-year').innerHTML = ("Name " + year);
+
+
